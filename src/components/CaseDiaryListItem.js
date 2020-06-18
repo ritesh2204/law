f95af7d783556f0ab141c4d8c6f90ae445823acb
ritesh2204 <rvritesh11@gmail.com>
2020-06-18 10:05:53 +0530

    Case diary list item fixed.

--- src/components/CaseDiaryListItem.js
+++ src/components/CaseDiaryListItem.js
@@ -6,7 +6,7 @@ const CaseDiaryListItem = () => {
     <div style={styles.panelMargin}>
       <div className="panel flex-container ">
         <div className="panel-head d-column " style={styles.iconCenter}>
-          <Image src="/images/court.png" circular className="img-80" />
+          <Image src="/images/court.png" circular className="img-50" />
           <p className="mb-0 mt-10 bold-green ">High Court</p>
         </div>
         <div className="d-column " style={{ flex: 6 }}>
@@ -18,20 +18,93 @@ const CaseDiaryListItem = () => {
         <div className="b-left"></div>
         <div className="d-column mt-5 " style={{ flex: 2 }}>
           <div className="mr">
-            <p className="lightgreen">
-              <b className="bold-green fontsize-20">NHD</b> 23 jun,2020
+            <p className="lightgreen f-24 mb-8">
+              <b className="bold-green">NHD</b> 23 jun,2020
             </p>
-            <p className="mb-0 lightgreen">
-              <b className="bold-green fontsize-20">LHD</b> 11 jun,2020
+            <p className="mb-0 lightgreen f-24">
+              <b className="bold-green ">LHD</b> 11 jun,2020
             </p>
           </div>
         </div>
+
+        <div style={{ ...styles.pillDarkGreen, display: "flex" }}>
+          <div style={styles.lightGreenPill}>
+            <strong style={styles.fs18}>CURRENT STAGE</strong>
+          </div>
+          <div
+            className="ml-10"
+            style={{
+              ...styles.fs16,
+              flex: 6,
+              padding: 15,
+              color: "#FFF",
+              marginLeft: 0,
+              background: "#1D9B9D",
+              borderRadius: "0px 50px 50px 0px",
+            }}
+          >
+            Evedence presentation
+          </div>
+        </div>
+
+        <div style={{ ...styles.pillDarkGreen, display: "flex" }}>
+          <div style={styles.lightGreenPill}>
+            <strong style={styles.fs18}>LAST STAGE</strong>
+          </div>
+          <div
+            className="ml-10"
+            style={{
+              ...styles.fs16,
+              flex: 6,
+              padding: 15,
+              color: "#FFF",
+              marginLeft: 0,
+              background: "#7B7D7D",
+              borderRadius: "0px 50px 50px 0px",
+            }}
+          >
+            Evedence presentation
+          </div>
+        </div>
+
+        {/* <div style={{ display: "flex" }}>
+          <div style={{ border: "1px solid red", flex: 1 }}>afkahfka</div>
+          <div style={{ border: "1px solid black", flex: 6 }}>asfasf</div>
+        </div> */}
       </div>
     </div>
   );
 };
 
 const styles = {
+  pillDarkGreen: {
+    borderRadius: 50,
+    marginTop: 8,
+    color: "#fff",
+    width: "100%",
+    display: "flex",
+  },
+  fs18: {
+    fontSize: 18,
+  },
+  fs16: {
+    fontSize: 18,
+  },
+  lightGreenPill: {
+    flex: 1,
+    padding: 15,
+    textAlign: "center",
+    borderRadius: "50px 0px 0px 50px",
+    color: "#FFF",
+
+    background: "#36A878",
+  },
+
+  roundTitle: {
+    background: "#000",
+  },
+  lightGrayPill: {},
+
   iconCenter: {
     display: "flex",
     flex: 1,
